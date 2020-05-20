319ca1fdb62fbd182c6a9081aa32295ac2faf44e
Negezor <negezor@gmail.com>
2020-05-20 21:46:03 +1100

    feat(test): add tests

--- test/inspectable.test.ts
+++ test/inspectable.test.ts
@@ -1,5 +1,48 @@
-describe('Coming', (): void => {
-	it('soon', (): void => {
+import { inspect } from 'util';
 
+import { inspectable } from '..';
+
+const createFixtureClass = (): new () => { method: string; token: string } => (
+	class Request {
+		public method = 'test';
+
+		public token = 'super-secret';
+	}
+);
+
+describe('inspectable', (): void => {
+	it('should be an empty object', (): void => {
+		const Klass = createFixtureClass();
+
+		inspectable(Klass);
+
+		expect(inspect(new Klass())).toStrictEqual('Request {}');
+	});
+
+	it('should have only one property', (): void => {
+		const Klass = createFixtureClass();
+
+		inspectable(Klass, {
+			serialize: instance => ({
+				method: instance.method
+			})
+		});
+
+		expect(inspect(new Klass())).toStrictEqual('Request {\n  method: \'test\'\n}');
+	});
+
+	it('should allow you to do your own stringify', (): void => {
+		const Klass = createFixtureClass();
+
+		inspectable(Klass, {
+			serialize: instance => ({
+				method: instance.method
+			}),
+			stringify: (instance, payload, context) => (
+				`Class [123] ${context.inspect(payload)}`
+			)
+		});
+
+		expect(inspect(new Klass())).toStrictEqual('Class [123] {\n  method: \'test\'\n}');
 	});
 });
